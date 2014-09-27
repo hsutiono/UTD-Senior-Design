11c9f1ca1f8801cb7ef514ed09c79d17cb8a50c6
hsutiono <hxs125130@utdallas.edu>
2014-09-27 13:34:22 -0500

    Sending a Message to Team Members
    Adding a capability to send a message to the team members with a button
    click

---
+++ SeniorDesign/Models/Team3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeniorDesign.Models
+{
+    public class Team3
+    {
+
+        public string number
+        {
+            get 
+            {
+                return number;
+            }
+            set { }
+        }
+        public class TeamMember
+        {
+            
+        }
+    }
+}
